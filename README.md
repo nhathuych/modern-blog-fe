a6dcd4491540c7153c459f0f2c5aafd802b188cf
Huy Huy <nhathuych@gmail.com>
2025-03-24 21:23:52 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# modern-blog-fe
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+```
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="text-orange-600">
+      Modern Blog
+    </div>
+  );
+}
